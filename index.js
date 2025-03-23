833e5592c8680edf67f6856b110b9e68d9ef76c7
Lean <leandrorodriguez68@gmail.com>
2025-03-23 22:09:25 +0000

    render deploy v7.2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World, api working on Render, v7!!!</h1>')
+  res.send('<h1>Hello World, api working on Render, v7.2!!!</h1>')
 })
 
 const PORT = 8080
